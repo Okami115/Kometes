257860136df9e9c7566d9914c66c4596313b51a7
OkamiDesu <tomaspp515@gmail.com>
2022-09-20 10:41:22 -0300

    Token Update

--- Kometes_PortePetitTomas.cpp
+++ Kometes_PortePetitTomas.cpp
@@ -22,9 +22,7 @@ int main()
     const int screenWidth = 1920;
     const int screenHeight = 1080;
 
-    InitWindow(screenWidth, screenHeight, "OkamiIndustries T - 03 || Kometes || V0.0");
-
-
+    InitWindow(screenWidth, screenHeight, "OkamiIndustries T - 03 || Kometes || V0.3");
 
     Vector2 v1 = { screenWidth / 2, 0 };
     Vector2 v2 = { 0 ,0 };
@@ -35,24 +33,19 @@ int main()
     Vector2 Origin = { Rec1.width / 2, Rec1.height / 2 };
 
     int rotated = 0;
-    SetTargetFPS(60);               
+    SetTargetFPS(60); 
+
     while (!WindowShouldClose()) 
     {
-        rotated++;
         MoveSpaceShip(Rec1);
 
         BeginDrawing();
 
         ClearBackground(BLACK);
-
-        //DrawTriangle(v1, v2, v3, WHITE);
         DrawRectanglePro(Rec1, Origin, rotated, ORANGE);
 
         EndDrawing();
-        
     }
 
-   
     CloseWindow();        
-
 }
