8d3aa2d01f71341cfbde4c97722afbc8f64e1490
OkamiDesu <Tomaspp515@gmail.com>
2022-09-21 23:46:35 -0300

    Upgrade SpaceShip Rotation and Fix Sprite

--- Kometes_PortePetitTomas.cpp
+++ Kometes_PortePetitTomas.cpp
@@ -3,10 +3,7 @@
 
 namespace OkamiIndustries
 {
-
-    
-
-    void MoveSpaceShip(Rectangle& SpaceShip)
+    void MoveSpaceShip(Vector2& SpaceShip, float& rotated)
     {
         Vector2 Cursor = GetMousePosition();
 
@@ -17,17 +14,24 @@ namespace OkamiIndustries
         Dif.x = SpaceShip.x - Cursor.x;
         Dif.y = SpaceShip.y - Cursor.y;
 
-        Vector2 DifNormalizated;
-        DifNormalizated.x = Dif.x / Vector2Length(Dif);
-        DifNormalizated.y = Dif.y / Vector2Length(Dif);
+        float arcTan = atan(Dif.y / Dif.x);
+
+        float angle = arcTan * 180 / PI;
+
+        float Module = sqrt(pow(Dif.x, 2) + pow(Dif.y, 2));
+
+        Vector2 normalDir = { Dif.x / Module, Dif.y / Module };
+
+        rotated = angle;
 
         if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
         {
-            SpaceShip.x -= DifNormalizated.x * aceleration * GetFrameTime();
-            SpaceShip.y -= DifNormalizated.y * aceleration * GetFrameTime();
+            SpaceShip.x -= normalDir.x * aceleration * GetFrameTime();
+            SpaceShip.y -= normalDir.y * aceleration * GetFrameTime();
         }
 
     }
+
 }
 
 int main()
@@ -37,26 +41,30 @@ int main()
 
     InitWindow(screenWidth, screenHeight, "OkamiIndustries T - 03 || Kometes || V0.05");
 
+    //SpaceShip
     Texture2D SpaceShip = LoadTexture("SpaceShip.png");
-
-    Rectangle Rec1 = { screenWidth / 2, screenHeight / 2, 110, 110 };
-
-    Vector2 Origin = { Rec1.width / 2, Rec1.height / 2 };
-    Vector2 SpritePos = { Rec1.x, Rec1.y };
-
+    Vector2 Origin = { SpaceShip.width / 2, SpaceShip.height / 2 };
+    Vector2 shipPosition = { screenWidth / 2, screenHeight / 2 };
+    Rectangle shipRectangle = { shipPosition.x, shipPosition.y, SpaceShip.width, SpaceShip.height };
     float rotated = 0;
+
+    
+
+    
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
-        OkamiIndustries::MoveSpaceShip(Rec1);
-        SpritePos = { Rec1.x, Rec1.y };
+
+        //SpaceShip Update
+        OkamiIndustries::MoveSpaceShip(shipPosition, rotated);
+        Rectangle destRec = { shipPosition.x, shipPosition.y, shipRectangle.width, shipRectangle.height };
 
         BeginDrawing();
 
         ClearBackground(BLACK);
-        DrawRectanglePro(Rec1, Origin, rotated, BLACK);
-        DrawTexture(SpaceShip, Rec1.x - Rec1.width / 2, Rec1.y - Rec1.height / 2, WHITE);
+
+        DrawTexturePro(SpaceShip, shipRectangle, destRec, Origin, rotated, WHITE);
 
         EndDrawing();
     }
